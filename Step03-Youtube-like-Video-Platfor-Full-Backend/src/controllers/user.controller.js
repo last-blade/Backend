890364f3bb801b845c28da058fad4477b9c82d39
Prashant <meprashanttyagi2000@gmail.com>
2025-02-23 20:32:58 +0530

    writing user-controller

---
+++ Step03-Youtube-like-Video-Platfor-Full-Backend/src/controllers/user.controller.js
@@ -0,0 +1,7 @@
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+const registerUser = asyncHandler(async (request, response) => {
+    response.status(200).json({
+        message: "OK",
+    })
+})
