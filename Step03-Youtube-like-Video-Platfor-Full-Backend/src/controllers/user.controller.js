563767de19bda02d8fece1ffeabbba9d0f2b896c
Prashant <meprashanttyagi2000@gmail.com>
2025-02-24 22:00:59 +0530

    added multer middleware at register route and writing user controller(in progress)

--- Step03-Youtube-like-Video-Platfor-Full-Backend/src/controllers/user.controller.js
+++ Step03-Youtube-like-Video-Platfor-Full-Backend/src/controllers/user.controller.js
@@ -1,9 +1,34 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
+import {apiError} from "../utils/apiError.js"
+import {User} from "../models/user.model.js"
 
 const registerUser = asyncHandler(async (request, response) => {
-    response.status(200).json({
-        message: "OK",
-    })
+    const {fullname, email, username, password} = request.body;
+
+    if([fullname, email, password, username].some((inputField) => inputField?.trim === "")){
+        /*
+            --> Array mein saare field ek baar mein bhej diye, jo fields user jo hai register karte time details fill karega,
+                and fir array par ".some" method laga diya jo ki boolean value return karta hai and callback fun accept
+                karta hai.
+            --> koi field empty hai ki nahin yeh sab toh hum "if-else" kaa use kar ke bhi kar sakte thay, lekin iske liye
+                fir humein multiple "if-else" lagane padte or har ek field ko check karte ki empty toh nahin hai koi field.    
+                
+            The "?."" (optional chaining) checks if inputField exists (i.e., not null or undefined) before 
+            accessing its properties. If inputField exists (not null or undefined), it calls .trim(). OR agar 
+            trime karne ke baad koi bhi filed khaali hai i.e. string empty(i.e. "") hai toh fir ".some" method jo hai 
+            true return kar dega ki bhai koi naa koi field khaali hai isliye true return kar diya ".some" method ne
+        */ 
+        throw new apiError(400, "All field are required")
+    }
+
+    const existedUser = User.findOne({
+        $or: [{ username }, { email }]
+    });
+
+    if(existedUser){
+        throw new apiError(409, "User with this email or username already exists.")
+    }
+
 })
 
 export {registerUser}
--- Step03-Youtube-like-Video-Platfor-Full-Backend/src/routes/user.routes.js
+++ Step03-Youtube-like-Video-Platfor-Full-Backend/src/routes/user.routes.js
@@ -1,10 +1,42 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js"
 
 const router = Router();
 
-router.route("/register").post(registerUser);
-console.log("User routes loaded!"); 
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1,
+        },
 
+        {
+            name: "coverImage", // name apne accoording de sakte hain
+            maxCount: 1 // jitni coverimage chahiye utni counting likh sakte hain yahan par
+        },
+    ]),
+    registerUser
+);
+
+/*
+    --> oopar wala code pehle aisa dikhta tha jab humne multe middleware nahin lagaya tha, see below:-
+        --> router.route("/register").post(registerUser); 
+        --> register route par registerUser wala controller chal raha tha, lekin ab "registerUser" controller 
+        chalne se pehle hum middleware chalayenge i.e. "multer", isliye "registerUser" se pehle multer laga 
+        diya i.e. "upload" jo humne multer middlware wali file se import kiya hai ooapr.
+        
+    --> ab aate hain next topic par, "upload.fields([])" kyon likha hai ?
+        Answer:- upload.fields([]) isliye likha hai kyoki humein user se multiple files chahiye, and jab bhi 
+                multiple files upload karwani hoti hai user se, toh hum ".fields" kaa use karte hain and woh multiple 
+                files hain "avatar" and "coverimage" and yeh files humein register karte time chahiye, "avatar and 
+                coverimage" kaa humne user model mein field banaya hua hai(see there).
+
+                --> And ".fields" array accept karta hai, and multiple files ke liye hum array ke andar objects bana sakte hain
+                    and jitni files chahiye utne objects.
+
+                --> single file upload ke liye hum "upload.single" kaa use karte hain.    
+
+*/
 
 export default router;
