e5af3570d2284feda02da0c8bb732bc37a539e20
Prashant <meprashanttyagi2000@gmail.com>
2025-03-05 21:06:09 +0530

    forgot to send response in  change-current-password-controller, now added send response code

--- Step03-Youtube-like-Video-Platfor-Full-Backend/src/controllers/userControllers/getCurrentUser.controller.js
+++ Step03-Youtube-like-Video-Platfor-Full-Backend/src/controllers/userControllers/getCurrentUser.controller.js
@@ -1,7 +1,7 @@
 import { asyncHandler, apiError, apiResponse, User } from "../allImports.js"
 
 const getCurrentUser = asyncHandler(async (request, response) => {
-    return response.status(200).json(200, request.user, "User fetched successfully")
+    return response.status(200).json(new apiResponse(200, request.user, "User fetched successfully"))
 });
 
 export {getCurrentUser}
