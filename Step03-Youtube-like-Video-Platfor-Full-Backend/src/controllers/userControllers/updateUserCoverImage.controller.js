33c85070bb40a55cfa42b3affc088f8c83615f59
Prashant <meprashanttyagi2000@gmail.com>
2025-03-05 20:55:16 +0530

    added update-cover-image-controller

---
+++ Step03-Youtube-like-Video-Platfor-Full-Backend/src/controllers/userControllers/updateUserCoverImage.controller.js
@@ -0,0 +1,31 @@
+import { asyncHandler, apiError, apiResponse, User, uploadOnCloudinary } from "../allImports.js"
+
+const updateUserCoverImage = asyncHandler(async (request, response) => {
+    const coverImageLocalPath = request.file?.path;
+    
+    if(!coverImageLocalPath){
+        throw new apiError(404, "Cover image file is missing")
+    }
+
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    if(!avatar.url){
+        throw new apiError(400, "Error while uploading cover image")
+    }
+
+    const user = User.findByIdAndUpdate(request.user?.id,
+        {
+            $set: {
+                coverImage: coverImage.url,
+            }
+        },
+
+        {new: true}// databse mein information update hone ke baad, updated user ko return karta hai, like user ne agar "fullname" update kiya hai, toh fir yeh jo hai "user" object ko return karega or usmein updated "fullname" hoga
+
+    ).select("-password");
+
+    return response.status(200)
+    .json(new apiResponse(200, user, "Cover image updated successfully"))
+});
+
+export {updateUserCoverImage}
